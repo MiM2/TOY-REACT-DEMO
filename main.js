48727e99c3d47856fccab10c0ffa84f18ed609ff
xiafeiyang <838900155@qq.com>
2020-08-11 09:23:06 +0800

    react 添加生命周期

--- main.js
+++ main.js
@@ -3,22 +3,50 @@ import {
     Component
 } from './ToyReact.js'
 
-class MyComponent extends Component{
-
-    render() {
-        return <div>
-            <span>HELLO</span>
-            <span>WORLD</span>
-            <div>
-                {this.children}
-            </div>
-        </div>
+class Square extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            value:null
+        }
+    }
+    render(){
+       return (
+          <button className="square" onClick={() => this.setState({value: 'X'})}>
+              {this.state.value?this.state.value:'' }
+          </button> 
+       );
     }
 }
 
-let demo = <MyComponent name = "test" id="idt">
-    <div>666</div>
-    </MyComponent>
+class Board extends Component{
+     renderSquare(i){
+         return <Square value={i} />
+     }
+     render(){
+        return (
+            <div>
+              <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+              </div>
+              <div className="board-row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+              </div>
+              <div className="board-row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+              </div>
+            </div>
+          );
+    }
+}
+
+let demo = <Board />
 
   ToyReact.render(
     demo,
